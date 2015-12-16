792b004762ba4fa7c7ccffab1fa724f139134b31
Julian Horvat <julian.m.horvat@gmail.com>
2015-12-16 14:34:21 -0500

    finally got the resume portion done

--- js/app.js
+++ js/app.js
@@ -1,2 +1,17 @@
 $(document).foundation();
-new WOW().init();
+// new WOW().init();
+
+$("#resumeLink").click(function(e) {
+    e.preventDefault();
+    $("#intro, #resume").toggleClass("hide");
+    // var a = $("#intro .small-11").get(0);
+    // var b = $("#resume .medium-4").get(0);
+    //
+    // ramjet.hide(a,b);
+    // ramjet.transform(a, b, {
+    //     done: function () {
+    //         ramjet.show(b);
+    //     }
+    // });
+
+});
